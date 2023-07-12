c91bde88a7f24c1d89398d6b5fa0d618cf850ae5
huanglallen <huanglallen@gmail.com>
2023-07-11 22:36:37 -0700

    Add User model scope for user security

---
+++ backend/db/seeders/20230712053011-demo-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo@user.io',
+        username: 'Demo',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'allen@gmail.com',
+        username: 'allen',
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], { validate: true });
+  },
+
+  async down (queryInterface, Sequelize) {
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'allen'] }
+    }, {});
+  }
+};
