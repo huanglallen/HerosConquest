f4704b23dfa0a635686d81f3df2ce387dd522b88
huanglallen <huanglallen@gmail.com>
2023-07-12 13:09:38 -0700

    fixed user migration file

--- backend/db/migrations/20230712052609-create-user.js
+++ backend/db/migrations/20230712052609-create-user.js
@@ -1,52 +1,48 @@
 'use strict';
-const { Model, Validator } = require('sequelize');
 
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    static associate(models) {
-      // define association here
-    }
-  };
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 
-  User.init(
-    {
-      username: {
-        type: DataTypes.STRING,
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
         allowNull: false,
-        validate: {
-          len: [4, 30],
-          isNotEmail(value) {
-            if (Validator.isEmail(value)) {
-              throw new Error("Cannot be an email.");
-            }
-          }
-        }
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        unique: true
       },
       email: {
-        type: DataTypes.STRING,
+        type: Sequelize.STRING(256),
         allowNull: false,
-        validate: {
-          len: [3, 256],
-          isEmail: true
-        }
+        unique: true
       },
       hashedPassword: {
-        type: DataTypes.STRING.BINARY,
+        type: Sequelize.STRING.BINARY,
+        allowNull: false
+      },
+      createdAt: {
         allowNull: false,
-        validate: {
-          len: [60, 60]
-        }
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
-    },
-    {
-      sequelize,
-      modelName: "User",
-      defaultScope: {
-        attributes: {
-          exclude: ["hashedPassword", "email", "createdAt", "updatedAt"]
-        }
-      }
-    }
-  );
-  return User;
+    }, options);
+  },
+
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Users";
+    return queryInterface.dropTable(options);
+  }
 };
