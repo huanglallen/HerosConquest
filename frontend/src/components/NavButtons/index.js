25827345f30ae0964638ab922371846117f76ce7
huanglallen <huanglallen@gmail.com>
2023-07-17 00:48:19 -0700

    navButtons added

--- frontend/src/components/NavButtons/index.js
+++ frontend/src/components/NavButtons/index.js
@@ -2,11 +2,13 @@ import { Link } from "react-router-dom";
 import "./NavButtons.css";
 
 const NavButtons = () => {
+    
     return (
         <div className="NavButtons">
             <div>Icon here</div>
             <div>Home</div>
-            <div></div>
+            <div>Heroes</div>
+            <div>Battles</div>
         </div>
     );
 };
