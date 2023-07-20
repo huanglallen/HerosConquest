9669f0de69b4a4b7dec70d4047ad86acde6aece1
huanglallen <huanglallen@gmail.com>
2023-07-20 14:07:49 -0700

    disabled navbuttons for no user

--- frontend/src/components/NavButtons/index.js
+++ frontend/src/components/NavButtons/index.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from "react-router-dom";
 import "./NavButtons.css";
@@ -7,12 +8,22 @@ const NavButtons = () => {
     const sessionUser = useSelector(state => state.session.user);
 
     return (
-        <div className="NavButtons">
-            <NavLink to='/home'>Home</NavLink>
-            <NavLink to='/heroes'>Heroes</NavLink>
-            <NavLink to='/battles'>Battle</NavLink>
-        </div>
+      <div className="NavButtons">
+        {sessionUser ? (
+          <>
+            <NavLink to='/home' className="navlink">Home</NavLink>
+            <NavLink to='/heroes' className="navlink">Heroes</NavLink>
+            <NavLink to='/battles' className="navlink">Battle</NavLink>
+          </>
+        ) : (
+          <>
+            <div className='navdisabled'>Home</div>
+            <div className='navdisabled'>Heroes</div>
+            <div className='navdisabled'>Battle</div>
+          </>
+        )}
+      </div>
     );
-};
+  };
 
 export default NavButtons;
