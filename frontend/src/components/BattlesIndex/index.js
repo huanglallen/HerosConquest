a0ebf3fdfcc181200fb96313f210423126d92900
huanglallen <huanglallen@gmail.com>
2023-07-23 01:59:48 -0700

    battleindex css added

--- frontend/src/components/BattlesIndex/index.js
+++ frontend/src/components/BattlesIndex/index.js
@@ -6,44 +6,47 @@ import "./BattlesIndex.css";
 
 
 const BattlesIndex = () => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const [disabled, setDisabled] = useState(false);
-    const battleArr = useSelector(state => state.battles?.battles?.battle);
-    const [battle] = battleArr || [];
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [disabled, setDisabled] = useState(false);
+  const battleArr = useSelector(state => state.battles?.battles?.battle);
+  const [battle] = battleArr || [];
 
-    useEffect(() => {
-        dispatch(getBattle());
-      }, [dispatch]);
+  useEffect(() => {
+      dispatch(getBattle());
+    }, [dispatch]);
 
-      useEffect(() => {
-        if (!battle) {
-          setDisabled(true);
-        } else {
-          setDisabled(false);
-        }
-      }, [battle]);
-
-    const handleNewBattle = () => {
-      if(battle) {
-        dispatch(deleteBattle(battle.id));
+  useEffect(() => {
+    if (!battle) {
+      setDisabled(true);
+    } else {
+       setDisabled(false);
       }
-        history.push('/battles/new')
-    };
+  }, [battle]);
 
-    const handleContinue = () => {
-        history.push(`battles/fight/${battle.id}`)
-    };
+  const handleNewBattle = () => {
+    if(battle) {
+      dispatch(deleteBattle(battle.id));
+    }
+      history.push('/battles/new')
+  };
 
-    return (
-        <div id="battleindex">
-            <h2>Start your battle</h2>
-            <button onClick={handleNewBattle}>New Battle</button>
-            <button
-            onClick={handleContinue}
-            disabled={disabled}
-            >Continue</button>
-        </div>
+  const handleContinue = () => {
+      history.push(`battles/fight/${battle.id}`)
+  };
+
+  return (
+    <div id="battleindex">
+      <h2 className="bi-title">START YOUR BATTLE</h2>
+      <button
+      className="bi-new"
+      onClick={handleNewBattle}>New Battle</button>
+      <button
+      className={battle ? "bi-continue" : "bi-continue-d"}
+      onClick={handleContinue}
+      disabled={disabled}
+      >Continue</button>
+    </div>
     );
 };
 
