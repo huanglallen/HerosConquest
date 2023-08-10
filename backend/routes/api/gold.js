925416f5311b99b8dc77af41032f21063b6603a1
huanglallen <huanglallen@gmail.com>
2023-08-10 16:45:55 -0700

    test

--- backend/routes/api/gold.js
+++ backend/routes/api/gold.js
@@ -5,4 +5,10 @@ const { requireAuth } = require('../../utils/auth');
 
 const { Gold } = require('../../db/models');
 
+router.get('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const userGold = await Gold.findOne()
+
+});
+
 module.exports = router;
