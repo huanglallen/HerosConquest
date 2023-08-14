8eba037b5928b5c64e2032c9d53ad6f6a5b7d7bc
huanglallen <huanglallen@gmail.com>
2023-08-13 21:57:10 -0700

    frontend App clean-up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
@@ -14,90 +14,6 @@ import BattleField from "./components/BattleField";
 function App() {
   const dispatch = useDispatch();
   const [isLoaded, setIsLoaded] = useState(false);
-
-  //Websocket Start
-
-  //username stuff === bananable
-  // const [username, setUsername] = useEffect('');
-  // const [messages, setMessages] = useState([]);
-  // const webSocket = useRef(null);
-
-  // useEffect(() => {
-  //   if(!username) return;
-
-  //   const ws = new WebSocket(process.env.REACT_APP_WS_URL);
-
-  //   ws.onopen = e => {
-  //     console.log(`Connection open: ${e}`);
-  //     setMessages([]);
-  //   };
-
-  //   ws.onerror = e => {
-  //     console.log(e);
-  //   };
-
-  //   ws.onclose = e => {
-  //     console.log(`Connection close: ${e}`);
-  //     webSocket.current = null;
-  //     setUsername('');
-  //     setMessages([]);
-  //   };
-
-  //   webSocket.current = ws;
-
-  //   return function cleanup() {
-  //     if(webSocket.current !== null) {
-  //       webSocket.current.close();
-  //     }
-  //   };
-  // }, [username]);
-
-  // //This effect is called whenever the 'messages' state is changed
-  // useEffect(() => {
-  //   if(webSocket.current !== null) {
-  //     //when changed, reassign 'onmessage' event listener
-  //     //to wrap around the updated state variable value
-  //     webSocket.current.onmessage = e => {
-  //       console.log(`Processing incoming message ${e.data}...`);
-
-  //       const chatMessage = JSON.parse(e.data);
-  //       const message = chatMessage.data;
-  //       message.created = new Date(message.created);
-
-  //       setMessages([message, ...messages])
-  //     };
-  //   };
-  // }, [messages]);
-
-  // const updateUsername = username => {
-  //   setUsername(username);
-  // };
-
-  // const handleSendMessage = message => {
-  //   const newMessage = {
-  //     id: uuid(),
-  //     username,
-  //     message,
-  //     created: new Date()
-  //   };
-
-  //   const jsonNewMessage = JSON.stringify({
-  //     type: 'send-chat-message',
-  //     data: newMessage
-  //   });
-
-  //   //test console.log
-  //   console.log(`Sending message: ${jsonNewMessage}...`);
-  //   webSocket.current.send(jsonNewMessage);
-
-  //   setMessages([newMessage, ...messages]);
-  // };
-
-  // const handleLeave = () => {
-  //   setUsername('');
-  // };
-
-  //WebSocket end
 
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
